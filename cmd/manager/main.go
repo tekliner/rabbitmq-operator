8531a17c7422fdd2caa935b74ea99d56d79bbcde
Vladimir Smagin <21h@blindage.org>
2019-03-21 14:21:45 +0700

    do not watch only operator's namespace

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -10,7 +10,6 @@ import (
 	"github.com/tekliner/rabbitmq-operator/pkg/apis"
 	"github.com/tekliner/rabbitmq-operator/pkg/controller"
 
-	"github.com/operator-framework/operator-sdk/pkg/k8sutil"
 	"github.com/operator-framework/operator-sdk/pkg/leader"
 	"github.com/operator-framework/operator-sdk/pkg/log/zap"
 	"github.com/operator-framework/operator-sdk/pkg/metrics"
@@ -59,11 +58,12 @@ func main() {
 
 	printVersion()
 
-	namespace, err := k8sutil.GetWatchNamespace()
-	if err != nil {
-		log.Error(err, "Failed to get watch namespace")
-		os.Exit(1)
-	}
+	// do not watch namespace
+	// namespace, err := k8sutil.GetWatchNamespace()
+	// if err != nil {
+	// 	log.Error(err, "Failed to get watch namespace")
+	// 	os.Exit(1)
+	// }
 
 	// Get a config to talk to the apiserver
 	cfg, err := config.GetConfig()
@@ -82,14 +82,22 @@ func main() {
 	}
 
 	// Create a new Cmd to provide shared dependencies and start components
-	mgr, err := manager.New(cfg, manager.Options{
-		Namespace:          namespace,
-		MetricsBindAddress: fmt.Sprintf("%s:%d", metricsHost, metricsPort),
-	})
+	mgr, err := manager.New(cfg, manager.Options{})
 	if err != nil {
 		log.Error(err, "")
 		os.Exit(1)
 	}
+
+	// do not watch namespace
+	// // Create a new Cmd to provide shared dependencies and start components
+	// mgr, err := manager.New(cfg, manager.Options{
+	// 	Namespace:          namespace,
+	// 	MetricsBindAddress: fmt.Sprintf("%s:%d", metricsHost, metricsPort),
+	// })
+	// if err != nil {
+	// 	log.Error(err, "")
+	// 	os.Exit(1)
+	// }
 
 	log.Info("Registering Components.")
 
